ca72ba5664333fe2508d448fb068e4ca3646f374
Arunarchitect <intformbase@gmail.com>
2024-01-14 22:32:23 +0530

    first commit

---
+++ frontend/client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './containers/HomePage';
+import DashboardPage from './containers/DashboardPage';
+import LoginPage from './containers/LoginPage';
+import RegisterPage from './containers/RegisterPage';
+import Test from './containers/Test';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {checkAuth} from './features/user'
+
+const App = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(checkAuth());
+	}, []);
+
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/dashboard' element={<DashboardPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/register' element={<RegisterPage />} />
+			</Routes>
+		</Router>
+	);
+};
+
+export default App;
---
+++ frontend/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const cookieParser = require('cookie-parser')
+
+const cors = require('cors');
+
+const path = require('path');
+
+require('dotenv').config();
+
+
+const registerRoute = require('./routes/auth/register');
+const loginRoute = require('./routes/auth/login');
+const meRoute = require('./routes/auth/me');
+const logoutRoute = require('./routes/auth/logout');
+const verifyRoute = require('./routes/auth/verify');
+
+
+
+
+const app = express();
+
+app.use(express.json())
+
+app.use(cors());
+app.use(cookieParser());
+
+// Use the route handlers for specific endpoints
+app.use(loginRoute);
+
+app.use(meRoute);
+app.use(registerRoute);
+app.use(logoutRoute);
+app.use(verifyRoute);
+
+app.use(express.static('client/dist'));
+app.get('*', (req, res) => {
+    const myPath = path.resolve(__dirname, 'client', 'dist', 'index.html');
+    console.log('__dirname',__dirname);
+    console.log('MyPath:',myPath);
+    return res.sendFile(myPath);
+});
+
+const PORT = process .env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+// app.get('*', (req, res) => {
+//     return res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+//     console.log('__dirname',__dirname);
+// })
