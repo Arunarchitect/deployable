0b9d2f86aac9ea6c8737c20355870887b80fbb24
Arunarchitect <intformbase@gmail.com>
2024-01-24 13:12:26 +0530

    addeddeploy settings

--- backend/authsite/settings.py
+++ backend/authsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rxes6_awe(6cr33g24tk==6ruroof&s@a2a10rf014ai1#hl#e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
